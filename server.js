d06c3e05ba1114435755bac15667541ad7c73b08
Flaming458 <86722150+Flaming458@users.noreply.github.com>
2021-11-24 16:08:27 +0100

    tentative n°3

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
 const http = require('http');
-//const Twit = require('twit');
 const config = require('./config');
+const { TwitterApi } = require('twitter-api-v2');
 
 const handleGetRequest = (req, res) => {
     
+
+  
     const options = {
         hostname: 'api.twitter.com',
-        path: '/2/tweets/search/recent?query=from:twitterdev',
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${config.bearer_token}`
-        }
+        path: '/2/tweets/search/recent?query=cat%20has%3Amedia%20-grumpy&tweet.fields=created_at&max_results=100',
+        method: 'GET'
+        // headers: {
+        //     'Authorization': `Bearer ${config.bearer_token}`
+        // }
     }
 
 
-    const request = http.request(options, (response) => {
+    const request = http.request(options, response => {
         let data = '';
 
         response.on('data', (chunk) => {
+            console.log(`Retrieved chunk: ${ chunk }`)
             data += chunk;
         });
 
         response.on('end', () => {
-            console.log(`Retrieved Data: ${ data }`);
+            console.table(`Retrieved Data: ${ data }`);
             res.end(data);
         });
     });
@@ -31,23 +34,21 @@ const handleGetRequest = (req, res) => {
 
 // Creates server instance
 const server = http.createServer((req, res) => {
-  const { method } = req;
+    const { method } = req;
  
-  
-
-  switch(method) {
-    case 'GET':
-      return handleGetRequest(req, res);
-    default:
-      throw new Error(`Unsupported request method: ${method}`);
-  }
+    switch(method) {
+        case 'GET':
+            return handleGetRequest(req, res);
+        default:
+            throw new Error(`Unsupported request method: ${method}`);
+    }
 });
 
 // Starts server listening on specified port
 server.listen(8080, () => {
-  const { port } = server.address();
+    const { port } = server.address();
 
-  console.log(`Serveur lancé: http://localhost:${port}`);
+    console.log(`Serveur lancé: http://localhost:${port}`);
 });
 
 
