c403cf2a5265c90aa9661a4f6815722778ec0383
Flaming458 <86722150+Flaming458@users.noreply.github.com>
2021-11-25 12:27:04 +0100

    Suppresion des données confidentielles

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key:         'AE6TA8FqY4n8feZYas4UWp7Rr',
-    consumer_secret:      'kAos3YwIxr1B7tRbFPxpDQkM0786LVJ7sSKOZlQpzLUqtE8fj3',
-    access_token:         '826881884684685312-iaBn8pwqRAW7GBiSAlRLDktNOqHjpKJ',
-    access_token_secret:  'pyLDpgWWYNaREPq6nBo4rk14Ba2b3qJY6B5bYuF4bcCTP',
-    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAEXYVwEAAAAA83DdLSrJgps3VCoSVjVSLXpxuYo%3DH81gLPj2yzIbzadWc23TyAFCuWgPnJHggcurIOJPA5J8H62D1M'
+    consumer_key:         'your consumer key',
+    consumer_secret:      'your secret consumer key',
+    access_token:         'your access token',
+    access_token_secret:  'your secret access token',
+    bearer_token: 'your bearer token'
 }
